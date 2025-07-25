541872e6ab2b2bba7771eb721a28812ccad6f4eb
Anish Mondal <134612111+CrAnish07@users.noreply.github.com>
2025-07-26 01:13:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (99.77%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> hashmap1;
-        unordered_map<char,int> hashmap2;
-
-        if (s.length() != t.length()) {
+        if(s.length() != t.length()) {
             return false;
         }
-
+        int hasharr[26] = {0};
         for(int i = 0; i < s.length(); i++) {
-            hashmap1[s[i]]++;
-            hashmap2[t[i]]++;
+            hasharr[s[i]-'a']++;
+            hasharr[t[i]-'a']--;
         }
-
-        for(const auto& pair: hashmap1) {
-            char key = pair.first;
-            int value = pair.second;
-
-            auto it = hashmap2.find(key);
-            if(it == hashmap2.end() || it->second != value) {
+        for(int i = 0; i < 26; i++) {
+            if(hasharr[i] != 0) {
                 return false;
             }
         }
